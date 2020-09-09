c62c67312eb3b39470be50a81994e9754a6ac94c
suraj gurung <sgrg007@gmail.com>
2020-09-09 00:58:13 -0500

    initial commit

---
+++ src/test/java/com/gurung/surajMaven1/HomePageTest.java
@@ -0,0 +1,31 @@
+package com.gurung.surajMaven1;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+  @Test
+  public void f1() {
+	  System.out.println("Printing: f1");
+  }
+  @Test
+  public void f2() {
+	  System.out.println("Printing: f2");
+  }
+  @Test
+  public void f3() {
+	  System.out.println("Printing: f3");
+  }
+  @Test
+  public void f4() {
+	  System.out.println("Printing: f4");
+  }
+  
+  @Test
+  public void f5() {
+	  System.out.println("Printing: f5");
+  }
+  @Test
+  public void f6() {
+	  System.out.println("Printing: f6");
+  }
+}
---
+++ src/test/java/com/gurung/surajMaven1/LoginPageTest.java
@@ -0,0 +1,47 @@
+package com.gurung.surajMaven1;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	
+	@Test
+	public void Test1() {
+		System.out.println("printing: Test1");
+	}
+	@Test
+	public void Test2() {
+		System.out.println("printing: Test2");
+	}
+	@Test
+	public void Test3() {
+		System.out.println("printing: Test3");
+	}
+	@Test
+	public void Test4() {
+		System.out.println("printing: Test4");
+	}
+	
+	@Test
+	public void Test5() {
+		System.out.println("printing: Test5");
+	}
+	@Test
+	public void Test6() {
+		System.out.println("printing: Test6");
+	}
+	@Test
+	public void Test7() {
+		System.out.println("printing: Test8");
+	}
+	@Test
+	public void Test8() {
+		System.out.println("printing: Test8");
+	}
+	
+	@Test
+	public void Test9() {
+		System.out.println("printing: Test9");
+	}
+
+}
